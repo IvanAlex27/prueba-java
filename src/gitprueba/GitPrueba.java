05d124b36213270c9378abebc752ca8ed123da74
Ivan Carrasco <icarrasco@externos.inami.gob.mx>
2015-03-20 11:56:34 -0600

    Que la fuerza los acompañe

---
+++ src/gitprueba/GitPrueba.java
@@ -0,0 +1,16 @@
+package gitprueba;
+
+/**
+ *
+ * @author iacarrasco
+ */
+public class GitPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
