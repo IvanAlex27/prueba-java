ad7b5006c021426b48f64e3616a083d07ee9b66c
cueho <ouriostegui@externos.inami.gob.mx>
2015-03-23 12:24:57 -0600

    Agregando yo soy cuecho

--- src/gitprueba/GitPrueba.java
+++ src/gitprueba/GitPrueba.java
@@ -11,6 +11,7 @@ public class GitPrueba {
      */
     public static void main(String[] args) {
         System.out.println("Hola bola de Cuechos ");
+        System.out.println("Yo soy Cuecho! ");
     }
     
 }
