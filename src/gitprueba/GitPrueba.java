939445642050b730c4472ce7a8d3c956b0214d72
Ivan Carrasco <icarrasco@externos.inami.gob.mx>
2015-03-23 09:48:13 -0600

    Que la fuerza los acompañe

--- src/gitprueba/GitPrueba.java
+++ src/gitprueba/GitPrueba.java
@@ -10,7 +10,7 @@ public class GitPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola bola de Cuechos");
+        System.out.println("Hola bola de Cuechos ");
     }
     
 }
